efc6ec73c5741f1b923b7134c06e67a87a57a359
Skythel <14922694+Skythel@users.noreply.github.com>
2023-05-22 13:42:02 -0400

    added models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    model = CarModel
+    list_display = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    
+    def __str__(self):
+       return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,7 +24,15 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    name = models.CharField(max_length=50)
+    dealerId = models.IntegerField()
+    cartype = models.CharField(max_length=50, choices=[("Sedan","Sedan"),("SUV","SUV"),("WAGON","WAGON")])
+    year = models.DateField()
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
 
+    def __str__(self):
+       return self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
